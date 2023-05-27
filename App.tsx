04312737257228882226b0821f51609c8eb1f384
Elvis <eleram@protonmail.com>
2023-05-27 20:06:34 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,86 @@
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Device } from "react-native-ble-plx";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { useBluetooth } from "./useBluetooth";
+
+export default function App() {
+  const {
+    requestPermissions,
+    scanForPeripherals,
+    allDevices: devices,
+    connectToDevice,
+    connectedDevice,
+    disconnectFromDevice,
+  } = useBluetooth();
+
+  console.log("Dispositivo Conectado:", connectedDevice);
+
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    console.log("isPermissionsEnabled?", isPermissionsEnabled);
+
+    if (isPermissionsEnabled) {
+      console.log("Scan UseEffect...");
+
+      scanForPeripherals();
+    }
+  };
+
+  const handleConnect = async (device: Device) => await connectToDevice(device);
+
+  const handleDisconnect = () => disconnectFromDevice();
+
+  useEffect(() => {
+    scanForDevices();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Lista de Dispositivos</Text>
+
+      {devices.map((device) => (
+        <TouchableOpacity
+          key={device.id}
+          onPress={() => handleConnect(device)}
+          style={styles.button}
+        >
+          <Text style={styles.text}>{device.name}</Text>
+        </TouchableOpacity>
+      ))}
+
+      <TouchableOpacity
+        onPress={() => handleDisconnect()}
+        style={styles.button}
+      >
+        <Text style={styles.text}>Deconectar</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 54,
+    width: "100%",
+    borderRadius: 8,
+    backgroundColor: "#7159c1",
+    marginTop: 24,
+  },
+
+  text: {
+    color: "#FFF",
+  },
+});
---
+++ useBluetooth.ts
@@ -0,0 +1,212 @@
+import { useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+
+import { Buffer } from "buffer";
+import * as ExpoDevice from "expo-device";
+import { BleManager, Device } from "react-native-ble-plx";
+
+const SERVICE_UUID = "0000ffff-0000-1000-8000-00805f9b34fb";
+const CHARACTERISTIC_UUID = "0000ff01-0000-1000-8000-00805f9b34fb";
+
+const HEADER = [0x4d, 0x00, 0x00, 0x2c];
+const PAYLOAD = { BT_PASSWORD: "93A400", GET_SERIAL_KEY: "" };
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(): Promise<boolean>;
+  scanForPeripherals(): void;
+  connectToDevice: (deviceId: Device) => Promise<void>;
+  disconnectFromDevice: () => void;
+  connectedDevice: Device | null;
+  allDevices: Device[];
+}
+
+function useBluetooth(): BluetoothLowEnergyApi {
+  const bleManager = useMemo(() => new BleManager(), []);
+  const [allDevices, setAllDevices] = useState<Device[]>(() => []);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+
+  const requestBluetoothScanPermission = async () => {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    return result === "granted";
+  };
+
+  const requestBluetoothConnectPermission = async () => {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    return result === "granted";
+  };
+
+  const requestFineLocationPermission = async () => {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    return result === "granted";
+  };
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermission === "granted" &&
+      bluetoothConnectPermission === "granted" &&
+      fineLocationPermission === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "Bluetooth Low Energy requires Location",
+            buttonPositive: "OK",
+          }
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const isDuplicateDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      console.log("Scan...");
+
+      if (error) console.error("scanForPeripherals()", error);
+
+      if (device && device.name?.includes("B2K")) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicateDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+        bleManager.stopDeviceScan();
+      }
+    });
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      const connectedDevice = await bleManager.connectToDevice(device.id);
+      setConnectedDevice(connectedDevice);
+
+      await discoverServicesAndCharacteristics(device);
+    } catch (error) {
+      console.error("Failed to connect:", error);
+    }
+  };
+
+  async function discoverServicesAndCharacteristics(device: Device) {
+    try {
+      const discoveredDevice =
+        await device.discoverAllServicesAndCharacteristics();
+
+      const services = await discoveredDevice.services();
+
+      const service = services.find((service) => service.uuid === SERVICE_UUID);
+
+      if (service) {
+        const characteristics = await service.characteristics();
+
+        const characteristic = characteristics.find(
+          (item) => item.uuid === CHARACTERISTIC_UUID
+        );
+
+        if (characteristic) {
+          let payloadString = JSON.stringify(PAYLOAD);
+
+          const payloadBuffer = Buffer.from(payloadString);
+
+          const concatenatedBuffer = Buffer.concat([
+            Buffer.from(HEADER),
+            payloadBuffer,
+          ]);
+          console.log("Concatenated Buffer", concatenatedBuffer);
+
+          const valueBase64 = concatenatedBuffer.toString("base64");
+          console.log("Value Base64:", valueBase64);
+
+          const response = await characteristic.writeWithResponse(valueBase64);
+          console.log("Response function writeWithResponse()", response);
+        }
+
+        if (!characteristic) console.log("characteristic not found!");
+      }
+
+      if (!service) console.log("Service not found!");
+    } catch (error) {
+      console.error("Error discovering services and characteristics:", error);
+    }
+  }
+
+  const disconnectFromDevice = () => {
+    if (connectedDevice) {
+      bleManager.cancelDeviceConnection(connectedDevice.id);
+      setConnectedDevice(null);
+    }
+  };
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    connectToDevice,
+    allDevices,
+    connectedDevice,
+    disconnectFromDevice,
+  };
+}
+
+export { useBluetooth };
